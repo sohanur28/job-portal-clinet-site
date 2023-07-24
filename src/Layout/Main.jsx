c1af890ab67bee164b8d7e330189b66d282c7d87
sohanur28 <gsrahman28@gmail.com>
2023-07-24 17:02:12 +0600

    layout & footer

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../shared/Footer/Footer";
+import NavBar from "../shared/NavBar/NavBar";
+
+const Main = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
